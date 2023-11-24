0679119469b3fd6c1665020ab6a0d454e2aed812
dlbrptrvc <37641298+dlbrptrvc@users.noreply.github.com>
2023-11-24 19:15:15 +0100

    Basic layout and slide menu

--- script.js
+++ script.js
@@ -1,40 +1,13 @@
-let ssb = document.getElementById('slidesb')
-let ct = document.getElementById('changetheme')
-let sb = document.querySelector(".sidebar")
-let body = document.body
-body.style.gridTemplate = "1fr / 300px 1fr"
-body.style.filter = 'invert(0%)'
+menubtn.addEventListener("click", () => {
+  dashboard.classList.toggle("slidout");
+});
 
-function checksize() {
-    if (screen.width<500) {ssb.click()}
-}
+menuclose.addEventListener("click", () => {
+  dashboard.classList.toggle("slidout");
+});
 
-ssb.addEventListener('click', ()=>{
-
-    let x = body.style.gridTemplate
-    if (x.includes('300px')) {
-        body.style.gridTemplate = "1fr / 1fr"
-        sb.style.display = 'none'
-    } else {
-        body.style.gridTemplate = "1fr / 300px 1fr"
-        sb.style.display = 'grid'
-    }
-})
-
-ct.addEventListener('click', ()=>{
-
-    let x = body.style.filter
-    if (x.includes('(0%)')) {
-        body.style.filter = "invert(100%)"
-        let avts = Array.from(document.querySelectorAll('[class^="avatar"]'))
-        avts.forEach( (item) => {
-            item.style.filter = "invert(100%)"
-        })
-    } else {
-        body.style.filter = "invert(0%)"
-        let avts = Array.from(document.querySelectorAll('[class^="avatar"]'))
-        avts.forEach( (item) => {
-            item.style.filter = "invert(0%)"
-        })
-    }
-})
+Array.from(dashboard.querySelectorAll(".ditem")).forEach((item) => {
+  item.addEventListener("click", () => {
+    dashboard.classList.toggle("slidout");
+  });
+});
